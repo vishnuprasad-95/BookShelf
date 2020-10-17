d17fbe928a4560a48229335e7ffe9a0f1a4a1d71
Ajay Sivan <ajaysivan@surveysparrow.com>
2020-10-18 02:58:02 +0530

    Delete functionality

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,31 @@
 import React, { useContext } from 'react';
 import { BookContext } from '../contexts/BookContext';
+import Icon from './common/Icon';
 
 const BookList = (props) => {
-  const { books, activeBook, setActiveBook } = useContext(BookContext);
+  const { books, activeBook, setActiveBook, removeBook } = useContext(BookContext);
 
   return books.length ? (
     <ul className="booklist">
       {books.map(book => {
         return (
-          <li key={book.id} className={`booklist__item ${book.id === activeBook ? 'booklist__item--active': ''}`} onClick={() => setActiveBook(book.id)}>
-            { book.title } 
+          <li key={book.id} className={`booklist__item ${book.id === activeBook ? 'booklist__item--active' : ''}`} onClick={() => setActiveBook(book.id)}>
+            <div className="booklist__title">
+              {book.title}
+            </div>
+            <button className="booklist__button" onClick={(e) => {
+              removeBook(book.id)
+              e.stopPropagation();
+            }}>
+              <Icon name="delete" size="16px" color="white" />
+            </button>
           </li>
         )
       })}
     </ul>
   ) : (
-    <div>Super Man book illa</div>
-  )
+      <div>Super Man book illa</div>
+    )
 }
 
 export default BookList;
